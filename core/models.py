d94c55ea3e68bd5b5b1a6ea564098579feea1799
Ricardo André <ricardo.andre.ifc@gmail.com>
2022-08-21 19:25:32 -0300

    continuando o projeto

--- core/models.py
+++ core/models.py
@@ -12,3 +12,14 @@ class Categoria(models.Model):
 
   def __str__(self):
     return self.descricao
+
+class Carro(models.Model):
+  modelo = models.CharField(max_length=50)
+  marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="carros")
+  categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="carros")
+  ano = models.IntegerField(null=True, blank=True)
+  cor = models.CharField(max_length=50, null=True, blank=True)
+  preco = models.FloatField(null=True, blank=True)
+
+  def __str__(self):
+    return f"{self.marca} {self.modelo} {self.cor} ({self.ano})"
