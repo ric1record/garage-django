8da6cd2dcfd6676da7350476d290ef9a8982265c
Ricardo André <ricardo.andre.ifc@gmail.com>
2022-08-21 19:15:19 -0300

    criando um segundo projeto

--- core/models.py
+++ core/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
-class Categoria(models.Model):
-  descricao = models.CharField(max_length=50)
-
-  def __str__(self):
-    return self.descricao
 
 class Marca(models.Model):
   nome = models.CharField(max_length=50)
 
   def __str__(self):
     return self.nome
+
+class Categoria(models.Model):
+  descricao = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.descricao
