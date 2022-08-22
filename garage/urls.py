d53ec867f6a898185b58f9f0cce77953dedfd678
Ricardo André <ricardo.andre.ifc@gmail.com>
2022-08-21 21:09:09 -0300

    api rest com drf

--- garage/urls.py
+++ garage/urls.py
@@ -3,10 +3,11 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from core.views import CategoriaViewSet
+from core.views import CategoriaViewSet, MarcaViewSet
 
 router = DefaultRouter()
 router.register(r"categorias", CategoriaViewSet)
+router.register(r"marcas", MarcaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
